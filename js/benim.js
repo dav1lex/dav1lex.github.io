083aa0351e72eea0fcb593ce7238841ec90c2cbb
Ömer <zedxurl@gmail.com>
2022-03-12 14:51:32 +0100

    c

--- js/benim.js
+++ js/benim.js
@@ -8,10 +8,3 @@ btn.onclick = function showorno (){
 		target.style.visibility = "hidden";
 	}		
 }
-
-$('.test, .nav-link, .navbar-brand, .new-button').click(function() {
-    var sectionTo = $(this).attr('href');
-    $('html, body').animate({
-      scrollTop: $(sectionTo).offset().top
-    }, 1500);
-});
