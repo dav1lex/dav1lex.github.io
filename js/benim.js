bec93722f9e4331ddd0a1d07db715b4691ec5e51
Ömer <zedxurl@gmail.com>
2022-03-13 15:24:59 +0100

    commitowanie

--- js/benim.js
+++ js/benim.js
@@ -1,10 +1,11 @@
 const target = document.getElementById('phone');
 const btn = document.getElementById('button');
-btn.onclick = function showorno (){
-	
-	if(target.style.visibility == "hidden"){
-		target.style.visibility = "visible";
-	}else{
-		target.style.visibility = "hidden";
-	}		
+btn.onclick = function showorno() {
+
+    if (target.style.visibility == "hidden") {
+        target.style.visibility = "visible";
+    } else {
+        target.style.visibility = "hidden";
+    }
 }
+
