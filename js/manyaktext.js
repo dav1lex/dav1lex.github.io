17d5d6e2ce21b221ff01be1d07fbe55f596d80e1
Ömer <79334944+dav1lex@users.noreply.github.com>
2022-03-15 23:55:23 +0100

    Update manyaktext.js

--- js/manyaktext.js
+++ js/manyaktext.js
@@ -1,16 +1,3 @@
-// var text = ["Welcome", "Hi", "Sup dude"];
-// var counter = 0;
-// var elem = document.getElementById("manyak");
-// var inst = setInterval(change, 1000);
-//
-// function change() {
-//     elem.innerHTML = text[counter];
-//     counter++;
-//     if (counter >= text.length) {
-//         counter = 0;
-//         // clearInterval(inst); // uncomment this if you want to stop refreshing after one cycle
-//     }
-// }
 
 var texts1 = ["Merhaba!", "Hello!", "Dzień Dobry!", "Hallo!"];
 var count = 0;
