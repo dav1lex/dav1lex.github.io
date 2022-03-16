157dbb3b0fabbd5408b12733eac1b932ba6980cb
Ömer <zedxurl@gmail.com>
2022-03-16 12:25:20 +0100

    commitowanie i pushowanie

--- js/manyaktext.js
+++ js/manyaktext.js
@@ -1,10 +1,11 @@
-
-var texts1 = ["Merhaba!", "Hello!", "Dzień Dobry!", "Hallo!"];
+var texts1 = ["Merhaba!", "Hello!", "Dzień Dobry!", "Здрастуйте", "Hallo!", "こんにちは","你好", "नमस्ते"];
 var count = 0;
 var eldz = document.getElementById('manyak');
+
 function changeText() {
 
     $("#manyak").text(texts1[count]);
-    count < 4 ? count++ : count = 0;
+    count < 8 ? count++ : count = 0;
 }
-setInterval(changeText, 1800);
+
+setInterval(changeText, 1500);
